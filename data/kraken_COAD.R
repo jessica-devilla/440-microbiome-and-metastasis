9d7fd3d2c22f0a9d050467450d1780cf02abc355
jessica-devilla <jessndev14@gmail.com>
2024-04-03 12:46:54 -0400

    add R dta files

---
+++ data/kraken_COAD.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9434c3fca48d704700be2ac43b70aca3b1ca3da4eaf8fa387c1dd81d5f3bfc4b
+size 9952485
---
+++ data/kraken_metaCOAD.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9efc2fd0d1796f78b31ae08e2b81f58c0e1cb35ae93de4b4a974b561d9f9b05
+size 124530
