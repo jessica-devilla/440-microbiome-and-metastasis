262cf17242b1ad14b6fe33f5ae6853a652b8886d
grace-goetz <gracegoetz1414@gmail.com>
2024-04-09 22:14:10 -0400

    Changes

--- code/TrackingPatternsAcrossMicrobiota.R
+++ code/TrackingPatternsAcrossMicrobiota.R
@@ -2,3 +2,15 @@
 kraken_metaCOAD_subset <- subset(kraken_metaCOAD, select = c("...1", "pathologic_stage_label"))
 merged_data_COAD <- merge(kraken_metaCOAD_subset, kraken_COAD, by = "...1")
 
+
+#cleaningdata
+subset_and_remove_column <- function(df, stage_labels) {
+  subset_data <- subset(df, pathologic_stage_label %in% stage_labels)
+  subset_data <- subset_data[, !names(subset_data) %in% "pathologic_stage_label"]
+  return(subset_data)
+}
+stageI_COADdata <- subset_and_remove_column(merged_data_COAD, c("Stage IA", "Stage IB", "Stage I"))
+stageII_COADdata <- subset_and_remove_column(merged_data_COAD, c("Stage IIA", "Stage IIB", "Stage II"))
+stageIII_COADdata <- subset_and_remove_column(merged_data_COAD, c("Stage IIIA", "Stage IIIB", "Stage III"))
+stageIV_COADdata <- subset_and_remove_column(merged_data_COAD, c("Stage IVA", "Stage IVB", "Stage IV"))
+
