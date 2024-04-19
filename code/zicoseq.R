89aba33ecf78d1e76a4ca8f12e6092e24725fa09
jessica-devilla <jessndev14@gmail.com>
2024-04-19 15:53:35 -0400

    zico seq updates

--- code/zicoseq.R
+++ code/zicoseq.R
@@ -1,31 +1,30 @@
-install.packages("GUniFrac")
+#install.packages("GUniFrac")
+
 library(GUniFrac)
 library(dplyr)
 
+####  load data
+kraken_filt_raw <- readRDS("data/kraken_raw_filtered.RDS")
+kraken_filt_norm <- readRDS("data/kraken_norm_filtered.RDS")
 
-#DataCleaning
-kraken_metaCOAD_subset <- subset(kraken_metaCOAD, sample_type == "Primary Tumor" & pathologic_stage_label != "Not available")
-
-
-
-kraken_metaCOAD_subset <- kraken_metaCOAD_subset[, c("...1", "pathologic_stage_label")]
-kraken_COAD_subset <- kraken_COAD %>%
-  filter(...1 %in% kraken_metaCOAD_subset$...1)
-
-row.names(kraken_COAD_subset) <- kraken_COAD_subset$...1
-kraken_COAD_subset$...1 <- NULL
+data(throat.otu.tab)
+data(throat.meta)
+comm <- t(throat.otu.tab)
+meta.dat <- throat.meta
+meta.dat
 
 
+kraken_filt$pathologic_stage_label <- gsub("Stage IV([A-C])?", "Stage IV", kraken_filt$pathologic_stage_label)
+kraken_filt$pathologic_stage_label <- gsub("Stage III([A-C])?", "Stage III", kraken_filt$pathologic_stage_label)
+kraken_filt$pathologic_stage_label <- gsub("Stage II([A-C])?", "Stage II", kraken_filt$pathologic_stage_label)
+kraken_filt$pathologic_stage_label <- gsub("Stage I([A-C])?", "Stage I", kraken_filt$pathologic_stage_label)
+row.names(kraken_filt) <- kraken_filt$...1
+kraken_filt$...1 <- NULL
 
-kraken_metaCOAD_subset$pathologic_stage_label <- gsub("Stage IV([A-C])?", "Stage IV", kraken_metaCOAD_subset$pathologic_stage_label)
-kraken_metaCOAD_subset$pathologic_stage_label <- gsub("Stage III([A-C])?", "Stage III", kraken_metaCOAD_subset$pathologic_stage_label)
-kraken_metaCOAD_subset$pathologic_stage_label <- gsub("Stage II([A-C])?", "Stage II", kraken_metaCOAD_subset$pathologic_stage_label)
-kraken_metaCOAD_subset$pathologic_stage_label <- gsub("Stage I([A-C])?", "Stage I", kraken_metaCOAD_subset$pathologic_stage_label)
-row.names(kraken_metaCOAD_subset) <- kraken_metaCOAD_subset$...1
-kraken_metaCOAD_subset$...1 <- NULL
+
 
 zicoseq_data <- t(kraken_COAD_subset)
-zico_test_1 <- ZicoSeq(meta.dat = kraken_metaCOAD_subset,  feature.dat = zicoseq_data, grp.name = 'pathologic_stage_label', adj.name = NULL, feature.dat.type = 'other', prev.filter = 0, mean.abund.filter = 0,  max.abund.filter = 0.002, min.prop = 0, 
+zico_test_1 <- ZicoSeq(meta.dat = kraken_filt,  feature.dat = zicoseq_data, grp.name = 'pathologic_stage_label', adj.name = NULL, feature.dat.type = 'other', prev.filter = 0, mean.abund.filter = 0,  max.abund.filter = 0.002, min.prop = 0, 
                        # Winsorization to replace outliers
                        is.winsor = TRUE, outlier.pct = 0.03, winsor.end = 'top',
                        # Posterior sampling to impute zeros
